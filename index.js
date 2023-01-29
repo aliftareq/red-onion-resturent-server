35e5c1581d7521e467c9b7e377e512f86a2912af
aliftareq <aliftareq@gmail.com>
2023-01-29 23:46:52 +0600

    jwt implemented

--- index.js
+++ index.js
@@ -31,8 +31,25 @@ async function run() {
 run().catch(err => console.log(err.message.red.bold))
 
 //collections
-const usersCollection = client.db('powerHack').collection('Users')
 const billingsCollection = client.db('powerHack').collection('billings')
+
+//common funcions 
+
+//1
+function verifyJwt(req, res, next) {
+    const authHeader = req.headers.authorization
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+    const token = authHeader.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden access' })
+        }
+        req.decoded = decoded
+        next()
+    })
+}
 
 //api's / endspoints
 
@@ -42,10 +59,9 @@ app.get('/', (req, res) => {
 })
 
 //api for getting billing list.
-app.get('/billing-list', async (req, res) => {
+app.get('/billing-list', verifyJwt, async (req, res) => {
     try {
         const page = req.query.page
-        console.log(page);
         const query = {}
         const billList = await billingsCollection.find(query).sort({ _id: -1 }).skip(page * 10).limit(10).toArray()
         const count = await billingsCollection.estimatedDocumentCount()
@@ -56,8 +72,21 @@ app.get('/billing-list', async (req, res) => {
     }
 })
 
+//api for getting a single bill 
+app.get('/bill', verifyJwt, async (req, res) => {
+    try {
+        const id = req.query.id
+        const query = { _id: ObjectId(id) }
+        const bill = await billingsCollection.findOne(query)
+        res.send(bill)
+    }
+    catch (error) {
+        res.send(error.message)
+    }
+})
+
 //api for posting single bookings of client
-app.post('/add-billing', async (req, res) => {
+app.post('/add-billing', verifyJwt, async (req, res) => {
     try {
         const bill = req.body
         const result = await billingsCollection.insertOne(bill)
@@ -66,6 +95,33 @@ app.post('/add-billing', async (req, res) => {
     catch (error) {
         console.log(error);
         res.send(error.message)
+    }
+})
+
+//api for deleting bill
+app.delete('/delete-billing/:id', verifyJwt, async (req, res) => {
+    try {
+        const id = req.params.id
+        const query = { _id: ObjectId(id) }
+        const result = await billingsCollection.deleteOne(query)
+        res.send(result)
+    }
+    catch (error) {
+        res.send(error.message)
+    }
+})
+
+//-----------------------JWT token ---------------------//
+
+//api for issue a access token
+app.get('/jwt', async (req, res) => {
+    try {
+        const email = req.query.email
+        const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '5h' })
+        res.send({ accessToken: token })
+    }
+    catch (error) {
+        res.send({ message: error.message })
     }
 })
 
